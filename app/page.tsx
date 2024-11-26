ff137dbdfbd8f99c59851f6cfffdd12bbeba71fc
majih93 <t90laser93@gmail.com>
2024-11-26 14:15:31 +0900

    3장: 이미지 최적화

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -30,6 +31,23 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          {/* width/height를 지정해서 layout shift를 방지하는 게 good practice */}
+          {/* width, height는 required property로, 생략이 불가능함 */}
+          {/* 사이즈에 따라서 자동으로 이미지가 교체되도록 설정되어 있음 */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
